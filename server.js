60a3be0460d9d238272fde5d0058885bf9343afd
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-16 16:23:29 +0000

    Added config data - hidden from git
    Updated server.js with databse information
    
    Updated post request for adding note

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
 const app = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
+// Needed as middle step for Express to be able to process URL encoded forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {  console.log('We are live on ' + port);});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  database = database.db("note-api");
+  require('./app/routes')(app, database);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
